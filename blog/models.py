f75828de07a52b7116e7e790392772c98e156c41
TatiPav <64731979+tatipav@users.noreply.github.com>
2021-11-09 20:19:18 +0300

    added models

---
+++ blog/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    )
+    title = models.CharField('Наименование', max_length=200)  #article title
+   # название = models.CharField(max_length=200)  # article title
+    slug = models.SlugField(max_length=200, unique_for_date='publish')
+    author = models.ForeignKey(User,on_delete=models.CASCADE,
+                               related_name='blog_posts')  #to generate URLs by dates
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now, unique_for_date='data_published')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+
+
