dc41fd4755048ded5506c70332dbbc0464fe2d8b
TatiPav <64731979+tatipav@users.noreply.github.com>
2021-11-11 19:55:10 +0300

    comma

--- blog/models.py
+++ blog/models.py
@@ -2,16 +2,22 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
-        ('published', 'Published')
+        ('published', 'Published'),
     )
-    title = models.CharField('Наименование', max_length=200)  #article title
-   # название = models.CharField(max_length=200)  # article title
+    title = models.CharField('Наименование', max_length=200)  # article title
+    # название = models.CharField(max_length=200)  # article title
     slug = models.SlugField(max_length=200, unique_for_date='publish')
-    author = models.ForeignKey(User,on_delete=models.CASCADE,
-                               related_name='blog_posts')  #to generate URLs by dates
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='blog_posts')  # to generate URLs by dates
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -24,4 +30,5 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-
+    objects = models.Manager()
+    published = PublishedManager()
